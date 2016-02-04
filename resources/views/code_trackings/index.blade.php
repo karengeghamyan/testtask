42dea6c47aff7ff0db12f8865b9ab82991d41b4b
dev <karen.geghamyan@gmail.com>
2016-02-04 18:45:25 +0400

    layout

--- resources/views/code_trackings/index.blade.php
+++ resources/views/code_trackings/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
 	<div class="container">
 	<div class="row">
 		<div class="col-md-6 col-md-offset-3">
@@ -102,5 +96,4 @@
 			background-color: blue;
 		}
 	</style>
-</body>
-</html>
+@stop
